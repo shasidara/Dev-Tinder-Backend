6e469814e87eff89dab5e6566cc314a111aaae6b
Shasidara <shasidara2@gmail.com>
2025-08-09 21:36:42 +0530

    exploring GET, POST, PATCH, DELETE

--- src/app.js
+++ src/app.js
@@ -2,13 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.patch("/user", (req, res) => {
+    res.send("Hello from patch request!");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Abaracadabra");
+})
+
+app.post("/user", (req, res) => {
+    res.send("Hello hello hello....");
+});
+
+app.get("/user", (req, res) => {
     res.send("Hello from dashboard!");
 });
 
-app.get("/hello", (req, res) => {
-    res.send("Hello hello hello....");
-});
 
 app.listen(2006, () => {
     console.log("server is successfully listening on port 2006");
